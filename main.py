8525458281a0dc8932c8343224acd2c29bd26bae
Can Küçükyılmaz <canthecomputerscientist@gmail.com>
2025-02-01 13:46:48 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.transform import *
+from src.utils import *
+from model.resnet_18 import *
 
-mean, std = compute_mean_std("input", for_training=False)
+def main():
+    config = load_config("config.yaml")
+    mean, std = compute_mean_std("input", for_training=False)
+    model = ResNet18()
+    model.load_state_dict(torch.load(config["model_path"]))
+    model.eval()
+
+if __name__ == "__main__":
+    main()
